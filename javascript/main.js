0a578719cff4b1671170f2676fad5fa4894a8152
Hugo Mendoza <thehugomendoza@gmail.com>
2023-12-10 18:36:54 -0800

    Made section One image scroll

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,24 @@
-console.log('Hello World')
+let background = document.querySelector(".background")
+
+let imageIndex = 2
+
+function changeBackground() {
+    // background.style.transition = 'opacity 500ms'
+
+    // background.style.opacity = '0'
+
+
+    background.src = `img/background${imageIndex}.jpg`
+
+
+    // background.style.opacity = '.5'
+
+
+    imageIndex++
+
+    if(imageIndex > 4){
+        imageIndex = 1
+    }
+}
+
+setInterval(changeBackground, 4000)
