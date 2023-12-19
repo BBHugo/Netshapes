005a34657eb0c384d62ea5b435adbf9b2cc00a45
Hugo Mendoza <thehugomendoza@gmail.com>
2023-12-18 20:08:43 -0800

    The fade out is working somewhat

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,8 @@ let background = document.querySelector(".background")
 
 document.querySelector('h5').addEventListener('click',showMarquee)
 
+
+//This is for the footer
 function showMarquee(){
     let marquee = document.querySelector('#marquee')
     if(marquee.style.display !== 'none'){
@@ -13,6 +15,8 @@ function showMarquee(){
     console.log('Website provided to Netshapes, Inc for usage by Hugo Mendoza. Any code provided by Hugo Mendoza remains under ownership of Hugo Mendoza unless otherwise stated in writing. Hello World! :)')
 }
 
+
+
 let imageIndex = 2
 
 function changeBackground() {
@@ -20,11 +24,14 @@ function changeBackground() {
 
     // background.style.opacity = '0'
 
-    let opacity = .8
+    // let opacity = .8
+    // background.style.opacity = opacity
+    //!!!!!OK! SO WE NEED TO FIND A WAY TO SET THE INTERVAL FOR THE FADE OUT OVER A SHORT PERIOD OF TIME INSTEAD OF INSTANT
 
-    
+    // while (opacity === .8){
+    //     opacity
+    // }
 
-     background.style.opacity = opacity
 
     background.src = `img/background${imageIndex}.jpg`
 
@@ -39,4 +46,29 @@ function changeBackground() {
     }
 }
 
-setInterval(changeBackground, 4000)
+// setInterval(changeBackground, 4000)
+
+
+
+//************************************************* */
+
+
+let imageIndex2 = 2
+let opacityVal = .8
+
+function changeBackground2() {
+
+    if(opacityVal === 0){
+        background.src = `img/background${imageIndex}.jpg`
+        imageIndex2++
+
+        if(imageIndex2 > 4){
+            imageIndex2 = 1
+        }
+    }
+    background.style.opacity = opacityVal
+    opacityVal -= .1
+
+}
+
+setInterval(changeBackground2, 200)
