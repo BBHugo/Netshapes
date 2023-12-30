7af9d2f707f9a5d70b9f98ee66dc0fe11e716974
Hugo Mendoza <thehugomendoza@gmail.com>
2023-12-29 18:48:04 -0800

    Adding class for lightDark

--- javascript/main.js
+++ javascript/main.js
@@ -46,53 +46,64 @@ function changeBackground() {
     }
 }
 
-// setInterval(changeBackground, 4000)
+setInterval(changeBackground, 4000)
 
 
 
-//************************************************* */
+//***************************The below are attempts to make a fade affect on pic but it isn't working by switchhing for some reason */
 
-let opacityVal = .8
-
-
-
-function changeBackground2() {
-    // let imageIndex2 = 2
-    let number = 1
-    // if(opacityVal > .8){
-    //     number = 1
-    // }else if(opacityVal < 0){
-    //     background.src = `img/background${imageIndex2}.jpg`
-    //     imageIndex2++
-    //     number = 2
-    //         if(imageIndex2 > 4){
-    //             imageIndex2 = 1
-    // }
-    // }else if(opacityVal > .8 ) {
-    //     number = 1
-    // }
-
-    if(number == 1){
-        opacityVal -= .1
-    }else if(number == 2){
-        opacityVal += .1
-    }
-
-    if(opacityVal < 0){
-        number = 2
-        console.log('switched to 2')
-    }else if(opacityVal >= .8){
-        number = 1
-        console.log('switched to 1')
-    }
-
-    console.log(`The number is ${number}`)
+// let opacityVal = .8
 
 
 
+// function changeBackground2() {
+//     // let imageIndex2 = 2
+//     let number = 1
+//     // if(opacityVal > .8){
+//     //     number = 1
+//     // }else if(opacityVal < 0){
+//     //     background.src = `img/background${imageIndex2}.jpg`
+//     //     imageIndex2++
+//     //     number = 2
+//     //         if(imageIndex2 > 4){
+//     //             imageIndex2 = 1
+//     // }
+//     // }else if(opacityVal > .8 ) {
+//     //     number = 1
+//     // }
 
-    background.style.opacity = opacityVal
+//     if(number == 1){
+//         opacityVal -= .1
+//     }else if(number == 2){
+//         opacityVal += .1
+//     }
+
+//     if(opacityVal < 0){
+//         number = 2
+//         console.log('switched to 2')
+//     }else if(opacityVal >= .8){
+//         number = 1
+//         console.log('switched to 1')
+//     }
+
+//     console.log(`The number is ${number}`)
+
+
+
+
+//     background.style.opacity = opacityVal
     
-}
+// }
 
-setInterval(changeBackground2, 200)
+// setInterval(changeBackground2, 200)
+
+//******************************Code below will be light Dark mode using OOPs */
+
+class LightDark{
+    constructor(body,header,button,sectionTwo){
+        this.body = document.querySelector('body')
+        this.header = document.querySelector('header')
+        this.button = document.querySelector('.lightDark')
+        this.sectionTwo = document.querySelector('.sectionTwo')
+    }
+}
